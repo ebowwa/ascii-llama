202785c18d21b38527a79104bb01b34201c58b87
ebowwa <ec.arbee1@gmail.com>
2024-05-11 12:19:29 -0700

    fix auth

--- training/sketchfab/auth.py
+++ training/sketchfab/auth.py
@@ -1,7 +1,11 @@
+import os
+
 __API_TOKEN = ""
 
-
-def set_api_token(api_token):
+def set_api_token():
     global __API_TOKEN
-    __API_TOKEN = api_token
-    print("Set API Token")
+    __API_TOKEN = os.getenv("API_TOKEN")
+    if __API_TOKEN:
+        print("Set API Token")
+    else:
+        print("API_TOKEN environment variable not set.")
